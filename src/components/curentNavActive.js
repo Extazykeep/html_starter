408381b2a648256d49e32e3dc14a83e235914fad
Extazykeep <Extazykeep@gmail.com>
2023-01-24 10:53:27 +0300

    updoot nav hightligting

--- src/components/curentNavActive.js
+++ src/components/curentNavActive.js
@@ -1,26 +1,27 @@
 const curentNavActive = () => {
     const navigationMenu = document.querySelector('.nav-fixed');
-    const navNodes = navigationMenu.querySelectorAll('.nav-fixed__link');
-    const sections = document.querySelectorAll('[data-observable]');
+    const navigationLinks = navigationMenu.querySelectorAll('.nav-fixed__link');
+    const observableSections = document.querySelectorAll('[data-observable]');
     const links = {};
 
-    navNodes.forEach((node) => {
-        links[node.getAttribute('href').substring(1)] = node;
+    navigationLinks.forEach((link) => {
+        links[link.getAttribute('href').substring(1)] = link;
     });
 
     const observer = new IntersectionObserver(observerCallback, {
         rootMargin: `-49.99% 0px`,
     });
-    sections.forEach((node) => {
-        observer.observe(node);
+    observableSections.forEach((section) => {
+        observer.observe(section);
     });
 
     function observerCallback(entries) {
         entries.forEach((entry) => {
-            if (navNodes) {
+            const intersectedId = entry.target.id;
+            if (navigationLinks) {
                 entry.isIntersecting
-                    ? links[entry.target.id].classList.add('active')
-                    : links[entry.target.id].classList.remove('active');
+                    ? links[intersectedId].classList.add('active')
+                    : links[intersectedId].classList.remove('active');
             }
         });
     }
